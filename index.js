e95dbf429bbed11bf388de2da69bd058a19182c9
HugoHern <hugo8954@gmail.com>
2022-09-04 19:40:48 -0400

    added cors middleware

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
+let cors = require('cors')
 
 //CONNECT TO DATABASE
 connectDB()
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 //TESTING LANDING PAGE 'LOCALHOST/5000/'
 app.get('/', (req, res) => res.send('Hello World!'))
